0728de3d07f0e0000f6c695e194cebd010569757
Stefan Steiner <ssteiner@berkeley.edu>
2016-09-21 16:01:38 -0700

    finish-hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,21 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    sum = 0
+    last = nil
+    a.sort!.each do |a|
+        int = a.to_i
+        int += 2
+        if int % 2 == 0
+            if int <= 10
+                if int != last
+                    sum += int
+                end
+            end
+        end
+        last = int
+    end
+    return sum
   end
 
   def foo(b)
